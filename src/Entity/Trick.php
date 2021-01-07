c3615a1a70b85d421241baa89f9b337ed5234a2a
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-01-07 17:28:20 +0100

    #13 add UniqueEntity 'title'

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -7,9 +7,11 @@ use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=TrickRepository::class)
+ * @UniqueEntity("title", message="Une fiche porte déjà ce nom")
  */
 class Trick
 {
