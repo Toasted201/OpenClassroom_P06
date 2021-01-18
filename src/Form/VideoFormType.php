441de1e990003849acbb70eb0259fa0ebc868d17
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-01-18 18:40:45 +0100

    #13 add regex 'contient embed' sur les url des vidéos

--- src/Form/VideoFormType.php
+++ src/Form/VideoFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class VideoFormType extends AbstractType
 {
@@ -15,9 +16,15 @@ class VideoFormType extends AbstractType
         $builder
         ->add('url', TypeTextType::class,[
             'label' => false,
-            'attr' => ['placeholder' => 'https://www.youtube.com/embed/........']
-        ])
-        ;
+            'attr' => ['placeholder' => 'Saisir un lien embed. ex : https://www.youtube.com/embed/........'],
+            'constraints' => [
+                new Regex([
+                    'pattern' => '/embed/',
+                    'match' => 'false',
+                    'message' => 'Le lien doit contenir "embed"'
+                ])
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
