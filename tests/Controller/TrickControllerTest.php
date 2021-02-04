fbd0871357cd1559478b8b659907a4f4aacb51bc
Toasted201 <69630035+Toasted201@users.noreply.github.com>
2021-02-04 10:53:30 +0100

    #30 add testAddComment

--- tests/Controller/TrickControllerTest.php
+++ tests/Controller/TrickControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TrickControllerTest extends WebTestCase
@@ -13,5 +14,24 @@ class TrickControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $this->assertSelectorTextContains('html h5', 'Description');
+    }
+
+    public function testAddComment()
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneBy(['email' => 'bob@doe.com']);
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/trick/mute');
+
+        $form = $crawler->selectButton('Valider')->form();
+        $form['comment_form[content]'] = 'Test Comment';
+        $crawler = $client->submit($form);
+
+        $client->followRedirect();
+
+        $this->assertSelectorTextContains('.text-info', 'Test Comment');
     }
 }    
